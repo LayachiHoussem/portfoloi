ed2d7cc74f630e33e2745bb44237612e410db0d4
layachi_houssem <129655521+hpusem@users.noreply.github.com>
2023-12-29 15:20:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text", {
+    strings: [ "Web Developer", "IT Engineer", "Network Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100  ,
+    backDelay: 2000,
+    loop: true
+  });
+
+ 
+  var oncli=alert.value("click")
